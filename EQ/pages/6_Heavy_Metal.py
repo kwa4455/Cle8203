8fea7c465628792093730f52feb3ef97a3fa56b6
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-01 14:54:44 +0000

    Update 6_Heavy_Metal.py

--- EQ/pages/6_Heavy_Metal.py
+++ EQ/pages/6_Heavy_Metal.py
@@ -454,8 +454,7 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
     df = cleaned(df)
     dfs = {}
-    for label, df in some_source.items():
-        value_cols = [col for col in df.columns if col.endswith('(ng/m3)') or col.endswith('(ug/m3)')]
+    value_cols = [col for col in df.columns if col.endswith('(ng/m3)') or col.endswith('(ug/m3)')]
     dfs[label] = df
     site_options.update(df['site'].unique())
     year_options.update(df['year'].unique())
